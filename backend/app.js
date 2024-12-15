fad53026356b2e5af24870a83f2533b605d2d3f9
Akshaya Krishna P <akshayakrishna.p03@gmail.com>
2024-12-15 23:50:33 +0530

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config');
+const bodyParser = require('body-parser');
+
+const userRoutes = require('./routes/userRoutes');
+const bookRoutes = require('./routes/bookRoutes');
+
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = config.PORT;
+const mongoDBURL = config.mongoDBURL;
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.use('/users', userRoutes);
+app.use('/books', bookRoutes);
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const User = require('../models/userModel');
+const router = express.Router();
+
+// Create a new user
+router.post('/signup', async (req, res) => {
+  const { name, email, password, role } = req.body;
+  try {
+    const newUser = new User({ name, email, password, role });
+    await newUser.save();
+    res.status(201).json({ message: 'User created successfully', user: newUser });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Get all users
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Get user by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Update user information
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Delete user
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    if (!deletedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json({ message: 'User deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
